441c06a3f3481415951fe22493bc3e2a9b7ac2d5
Rachel <37462928+rugosum@users.noreply.github.com>
2018-03-26 15:45:41 -0400

    Add files via upload

---
+++ checkout.java
@@ -0,0 +1,16 @@
+package testcases;
+
+//extends ModuleMethods, no import needed here 
+
+public class checkout extends ModuleMethods{
+	public static void main(String[] args) {
+		ModuleMethods modules = new ModuleMethods();
+		ModuleMethods.main(args);
+		modules.openNewEgg();
+		/*Copy above 4 lines for new test cases*/
+		
+		modules.search();	
+	}
+	
+
+}
---
+++ guestcheckout.java
@@ -0,0 +1,14 @@
+package testcases;
+
+//extends ModuleMethods, no import needed here
+
+public class guestcheckout extends ModuleMethods{
+	public static void main(String[] args) {
+		ModuleMethods modules = new ModuleMethods();
+		ModuleMethods.main(args);
+		modules.openNewEgg();
+		/*Copy above 4 lines for new test cases*/
+		
+		modules.search();		
+	}
+}
